c73cf9371e6dc74344c92a6b23fa71f5fb0e128e
Gursharan Singh <gursharansingh4312@gmail.com>
2019-08-26 02:23:23 -0700

    particle js background added

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,9 +1,33 @@
 import React from "react";
 import "./HeroSection.scss";
+import Particles from "react-particles-js";
 
 export default function HeroSection() {
+  const particles = {
+    particles: {
+      number: {
+        value: 500
+      },
+      line_linked: {
+        enable: false
+      },
+      size: {
+        value: 3,
+        anim: {
+          enable: true,
+          speed: 2
+        }
+      }
+    },
+    interactivity: {
+      onhover: {
+        enable: true
+      }
+    }
+  };
   return (
     <div className="hero-section">
+      <Particles width="100%" height="90vh" params={particles} />
       <div className="hero-content">
         <h1>
           Hi, I'm <span>Gursharan Singh</span>
